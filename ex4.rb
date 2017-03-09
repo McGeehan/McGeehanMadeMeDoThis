28ae0360f0871d5e797b19d2579ea48390f207e6
Matt <mmcgeehan@paduaacademy.org>
2017-03-09 12:50:40 -0500

    Add files via upload

---
+++ ex4.rb
@@ -0,0 +1,22 @@
+my_name = 'Matthias Mcgee' #shortened
+
+my_age = 48 #lie
+
+my_height = 68 #in shoes
+
+my_weight = 142 #before eating anything
+
+my_eyes = 'Brown'
+
+my_teeth = 'Mostly not fake'
+
+my_hair = 'Red'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the day."
+
+puts "If I add #{my_age}, #{my_weight}, and #{my_height} I get #{my_age + my_height + my_weight}."
